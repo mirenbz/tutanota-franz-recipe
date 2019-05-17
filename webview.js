159173930d6cda36af10c0ac888e748becde1540
mberasategi <miren.berasategi@gmail.com>
2019-05-17 10:08:57 +0200

    Count only unread items from Inbox

--- webview.js
+++ webview.js
@@ -1,6 +1,6 @@
 module.exports = Franz => {
     function getMessages() {
-        const count = document.querySelector('.folder-counter').innerText
+        const count = document.querySelector('div.folder-row:first-child .folder-counter').innerText
         Franz.setBadge(count ? Number(count.substring(1, count.length - 1)) : 0)
     }
     Franz.loop(getMessages)
