405f2fca34924acc6f97ef27c9e3b008a0000572
miren <mail@miren.be>
2020-04-13 15:27:40 +0200

    Update

--- webview.js
+++ webview.js
@@ -1,3 +1,12 @@
+// module.exports = Franz => {
+//     function getMessages() {
+//         const count = document.querySelector('div.folder-row:first-child .folder-counter').innerText
+//         Franz.setBadge(count ? Number(count.substring(1, count.length - 1)) : 0)
+//     }
+//     Franz.loop(getMessages)
+// }
+
+
 module.exports = Franz => {
     function getMessages() {
         const count = document.querySelector('div.folder-row:first-child .folder-counter').innerText
@@ -5,3 +14,6 @@ module.exports = Franz => {
     }
     Franz.loop(getMessages)
 }
+// var myElem = document.getElementById('myElementId');
+// if (myElem === null) alert('does not exist!');
+
